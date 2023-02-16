941357939a8a2ecaaa16ab20dae2e36703513462
birajchhetri30 <birajchhetri3824@gmail.com>
2023-02-17 00:57:13 +0530

    FTP for intruder photo transfer and download
    When intruder is detected, photo will be uploaded on a server by the host (Pi).
    
    For the App, a client program will download the photo and place it in a proper folder with folder_name = date of photo taken.
    
    Gmail OAuth implementation no more

---
+++ client.py
@@ -0,0 +1,33 @@
+import paramiko
+import socket
+from ftplib import FTP
+import datetime
+import os
+
+
+def run():
+    ftp = FTP()
+    server = "ftp.epizy.com"
+    port = 21
+    ftp.connect(server, port)
+    ftp.login("epiz_33608356", "HwGN8xvq7ut")
+
+    files = ftp.nlst()  # list of files on the server
+    #print(files)
+
+    for file in files:
+        if file[-4:] == ".jpg":
+            date = file[:file.find("_")]  # folder name will be the date
+            if not date in os.listdir() or not file in os.listdir(date):  # if photo does not exist
+                if not os.path.isdir(date):
+                    os.mkdir(date)
+
+                path = os.path.join(date, file)
+                r = ftp.retrbinary('RETR %s' % file, open(path, 'wb').write)
+                print(r)
+            else:
+                print("File already downloaded")
+
+
+if __name__ == '__main__':
+    run()
