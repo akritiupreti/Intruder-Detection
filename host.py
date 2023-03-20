e568ff272d1301138acb528c5b4fea6d90e2dca3
birajchhetri30 <birajchhetri3824@gmail.com>
2023-03-20 19:53:38 +0530

    Added getFlag which waits for owner's response

--- host.py
+++ host.py
@@ -1,30 +1,53 @@
 from ftplib import FTP
+import time
 
 
 class Host:
     def __init__(self):
         self.ftp = FTP()
-        server = "ftp.epizy.com"
-        port = 21
-        self.ftp.connect(server, port)
+        self.server = "ftp.epizy.com"
+        self.port = 21
+        self.ftp.connect(self.server, self.port)
         self.ftp.login("epiz_33608356", "HwGN8xvq7ut")
 
     def getStatus(self):
         files = self.ftp.nlst()
-        self.ftp.quit()
         if "on.txt" in files:
             return True
         else:
             return False
 
+    def getFlag(self):
+        files = self.ftp.nlst()
+        while True:  # wait for user to configure settings in app
+            if "incomplete.txt" in files:
+                files = self.ftp.nlst()
+                time.sleep(5)
+            else:
+                break
+
+        self.ftp.rename("complete.txt", "incomplete.txt")
+
+        if "00.txt" in files:
+            return "00"
+        elif "01.txt" in files:
+            return "01"
+        else:
+            name = None
+            for file in files:
+                if file[:5] == "name_":
+                    name = file[5:]
+            return "11", name
+
     def run(self, file, filename):
-        files = self.ftp.nlst() #list of files on the server
+        #files = self.ftp.nlst() #list of files on the server
         #print(files)
 
         filename = filename.replace(":", ".")
         r = self.ftp.storbinary('STOR %s' % filename, file)
         #print(r)  # should be: 226 Transfer OK
 
+    def closeConnection(self):
         self.ftp.quit()
 
 
